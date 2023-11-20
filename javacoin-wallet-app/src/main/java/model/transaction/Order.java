b5fa1912ae73bfd7a914527de292af9674a75f1e
iggalvez2023 <iggalvez2023@gmail.com>
2023-11-20 16:59:08 -0300

    - Modified OrderService added more validations - Modified Order refactored code - AlreadyProcessedOrderException added

---
+++ javacoin-wallet-app/src/main/java/model/transaction/Order.java
@@ -0,0 +1,21 @@
+package model.transaction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import model.transaction.enums.Status;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@SuperBuilder
+public class Order  {
+    private final UUID orderUUID;
+    private final BigDecimal amount;
+    private final BigDecimal price;
+    private final String bidderUsername;
+    private final String offereeUsername;
+    private Status status;
+}
--- javacoin-wallet-app/src/main/java/service/OrderService.java
+++ javacoin-wallet-app/src/main/java/service/OrderService.java
@@ -1,5 +1,6 @@
 package service;
 
+import exception.AlreadyProcessedOrderException;
 import model.User;
 import model.Wallet;
 import model.transaction.Order;
@@ -7,15 +8,26 @@ import model.transaction.enums.Status;
 
 public class OrderService {
 
-    public Order validateAndProcessOrder(Order order) {
+    public Order validateAndProcessOrder(Order order) throws AlreadyProcessedOrderException {
+        validateOrder(order);
         User user = UserService.findUser(order.getOffereeUsername());
         Wallet wallet = user.getWallet();
+        AcceptOrRejectOrder(order, wallet);
+        return order;
+    }
+
+    private void AcceptOrRejectOrder(Order order, Wallet wallet) {
         if(wallet.getAmount().subtract(order.getAmount()).signum() >= 0 ) {
             order.setStatus(Status.ACCEPTED);
 
         } else {
             order.setStatus(Status.REJECTED);
         }
-        return order;
+    }
+
+    private void validateOrder(Order order) throws AlreadyProcessedOrderException {
+        if(order.getStatus().equals(Status.REJECTED) || order.getStatus().equals(Status.EXECUTED)){
+            throw new AlreadyProcessedOrderException();
+        }
     }
 }
