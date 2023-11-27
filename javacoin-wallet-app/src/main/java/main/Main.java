b99b05c116dbc5ccce2e27b222245abd8ceb7ca9
iggalvez2023 <iggalvez2023@gmail.com>
2023-11-27 19:10:55 -0300

    - commit

---
+++ javacoin-wallet-app/src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import consumer.OrderConsumer;
+
+import javax.naming.NamingException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws NamingException {
+
+        OrderConsumer orderConsumer = new OrderConsumer();
+
+        orderConsumer.consume();
+        orderConsumer.consume();
+    }
+}
